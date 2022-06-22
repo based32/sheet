b258f9177e065e24cbecaabf59f71b99af43f455
Based 32 <based322@protonmail.com>
2022-06-23 02:42:29 +0300

    wip

--- selections/src/movements.rs
+++ selections/src/movements.rs
@@ -1,3 +1,10 @@
-use crate::SelectionStorage;
+use crate::{Position, SelectionDeltas, SelectionStorage};
 
-impl SelectionStorage {}
+impl SelectionStorage {
+    /// Moves a selection identified by `pos` as its `from` position on `n`
+    /// columns. Will be narrowed to length of 1 character if `extend` is
+    /// `false`.
+    pub fn move_left_single(&mut self, pos: &Position, n: usize, extend: bool) -> SelectionDeltas {
+        todo!()
+    }
+}
