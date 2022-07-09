9ceaf7884199a85d056c02f9bd5b3a405313d270
Based 32 <based322@protonmail.com>
2022-07-09 20:54:00 +0300

    wip

--- selections/src/movements.rs
+++ selections/src/movements.rs
@@ -3,11 +3,55 @@ mod tests;
 
 use crate::{Position, SelectionDeltas, SelectionStorage};
 
+/// Source of line lengthes for a buffer.
+pub trait LineLength {
+    /// Returns a length for a line specified by its index.
+    fn get_len(&self, line: usize) -> usize;
+}
+
+impl Position {
+    fn move_left(&self, line_lengths: impl LineLength, mut n: usize) -> Position {
+        let mut new_pos = self.clone();
+        while n > 0 {
+            if new_pos.column < n {
+                // If there is no space to move left then the position is a beginning of a buffer.
+                if new_pos.line == 0 {
+                    new_pos.column = 0;
+                    break;
+                }
+
+                n -= new_pos.column;
+                new_pos.line -= 1;
+                new_pos.column = line_lengths.get_len(new_pos.line);
+            }
+        }
+        new_pos
+    }
+
+    fn move_right(&self, line_lengths: impl LineLength, n: usize) -> Position {
+        todo!()
+    }
+
+    fn move_up(&self, line_lengths: impl LineLength, n: usize) -> Position {
+        todo!()
+    }
+
+    fn move_down(&self, line_lengths: impl LineLength, n: usize) -> Position {
+        todo!()
+    }
+}
+
 impl SelectionStorage {
     /// Moves a selection identified by `pos` as its `from` position on `n`
     /// columns. Will be narrowed to length of 1 character if `extend` is
     /// `false`.
-    pub fn move_left_single(&mut self, pos: &Position, n: usize, extend: bool) -> SelectionDeltas {
+    pub fn move_left_single(
+        &mut self,
+        line_lengths: impl LineLength,
+        pos: &Position,
+        n: u32,
+        extend: bool,
+    ) -> SelectionDeltas {
         todo!()
     }
 }
---
+++ selections/src/movements/tests/position.rs
@@ -0,0 +1,18 @@
+use crate::{util::selections_test, Position};
+
+mod move_left {
+    #[test]
+    fn one_line() {
+
+    }
+
+    #[test]
+    fn multiple_lines() {
+
+    }
+
+    #[test]
+    fn hit_buffer_beginning() {
+
+    }
+}
