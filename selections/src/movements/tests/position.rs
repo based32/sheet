255f289dfaada787ae045c5caa83b2355a8989f2
Based 32 <based322@protonmail.com>
2022-07-31 18:40:53 +0300

    implement position right movement

--- selections/src/movements/tests/position.rs
+++ selections/src/movements/tests/position.rs
@@ -18,14 +18,13 @@ mod move_left {
         line_lengths.set(1, 20);
 
         let pos = Position::new(1, 15).move_left(&line_lengths, 16);
-        // Line length is 10 so last char is 9, but position is line end.
-        assert_eq!(pos, Position::new(0, 10));
+        assert_eq!(pos, Position::new(0, 9));
 
-        // xxxxxxxxxx_ - 10
-        // xxxxxxxxxxxxxxxxxxxx_ - 20
+        // xxxxxxxxx_ - 10
+        // xxxxxxxxxxxxxxxxxxx_ - 20
         // xxxxx|x| - 6
         let pos = Position::new(2, 5).move_left(&line_lengths, 32);
-        assert_eq!(pos, Position::new(0, 5));
+        assert_eq!(pos, Position::new(0, 3));
     }
 
     #[test]
@@ -35,6 +34,60 @@ mod move_left {
         line_lengths.set(1, 20);
 
         let pos = Position::new(2, 5).move_left(&line_lengths, 69);
+        assert_eq!(pos, Position::new(0, 0));
+    }
+
+    #[test]
+    fn test_empty_buffer() {
+        let line_lengths = TestLineLengths::default();
+        let pos = Position::new(0, 0).move_left(&line_lengths, 69);
+        assert_eq!(pos, Position::new(0, 0));
+    }
+}
+
+mod move_right {
+    use super::*;
+
+    #[test]
+    fn one_line() {
+        let mut line_lengths = TestLineLengths::default();
+        line_lengths.set(1, 30);
+        let pos = Position::new(1, 10).move_right(&line_lengths, 5);
+        assert_eq!(pos, Position::new(1, 15));
+    }
+
+    #[test]
+    fn multiple_lines() {
+        let mut line_lengths = TestLineLengths::default();
+        line_lengths.set(0, 10);
+        line_lengths.set(1, 20);
+        line_lengths.set(2, 6);
+
+        let pos = Position::new(0, 5).move_right(&line_lengths, 15);
+        assert_eq!(pos, Position::new(1, 10));
+
+        // xxxxxxxxx_ - 10
+        // xxxxxxxxxxxxxxxxxxx_ - 20
+        // xxxxx|x| - 6
+        let pos = Position::new(0, 0).move_right(&line_lengths, 32);
+        assert_eq!(pos, Position::new(2, 2));
+    }
+
+    #[test]
+    fn hit_buffer_end() {
+        let mut line_lengths = TestLineLengths::default();
+        line_lengths.set(0, 10);
+        line_lengths.set(1, 20);
+        line_lengths.set(2, 6);
+
+        let pos = Position::new(0, 5).move_right(&line_lengths, 69);
+        assert_eq!(pos, Position::new(2, 5));
+    }
+
+    #[test]
+    fn test_empty_buffer() {
+        let line_lengths = TestLineLengths::default();
+        let pos = Position::new(0, 0).move_right(&line_lengths, 69);
         assert_eq!(pos, Position::new(0, 0));
     }
 }
