a0ba3680a66497d222199a46d71f4c8013f29f70
Based 32 <based322@protonmail.com>
2022-08-15 20:03:47 +0300

    position move up

--- selections/src/movements/tests/position.rs
+++ selections/src/movements/tests/position.rs
@@ -58,7 +58,7 @@ mod move_left {
     }
 
     #[test]
-    fn test_empty_buffer() {
+    fn empty_buffer() {
         let line_lengths = TestLineLengths::new();
         let pos = Position::new(0, 0).move_left(&line_lengths, 69);
         assert_eq!(pos, Position::new(0, 0));
@@ -124,9 +124,87 @@ mod move_right {
     }
 
     #[test]
-    fn test_empty_buffer() {
+    fn empty_buffer() {
         let line_lengths = TestLineLengths::new();
         let pos = Position::new(0, 0).move_right(&line_lengths, 69);
         assert_eq!(pos, Position::new(0, 0));
+    }
+}
+
+mod move_up {
+    use super::*;
+
+    #[test]
+    fn one_line() {
+        let mut line_lengths = TestLineLengths::new();
+        line_lengths.set(0, 30);
+        line_lengths.set(1, 30);
+        let pos = Position::new(1, 10).move_up(&line_lengths, 1);
+        assert_eq!(pos, Position::new(0, 10));
+    }
+
+    #[test]
+    fn multiple_lines() {
+        let mut line_lengths = TestLineLengths::new();
+        line_lengths.set(0, 10);
+        line_lengths.set(1, 20);
+        line_lengths.set(2, 6);
+
+        let pos = Position::new(2, 6).move_up(&line_lengths, 2);
+        assert_eq!(pos, Position::new(0, 6));
+    }
+
+    #[test]
+    fn multiple_lines_with_empty() {
+        let mut line_lengths = TestLineLengths::new();
+        line_lengths.set(0, 10);
+        line_lengths.set(1, 0);
+        line_lengths.set(2, 20);
+        line_lengths.set(3, 6);
+
+        let pos = Position::new(3, 6).move_up(&line_lengths, 3);
+        assert_eq!(pos, Position::new(0, 6));
+    }
+
+    #[test]
+    fn hit_buffer_beginning() {
+        let mut line_lengths = TestLineLengths::new();
+        line_lengths.set(0, 10);
+        line_lengths.set(1, 20);
+        line_lengths.set(2, 6);
+
+        let pos = Position::new(2, 5).move_up(&line_lengths, 69);
+        assert_eq!(pos, Position::new(0, 5));
+    }
+
+    #[test]
+    fn empty_buffer() {
+        let line_lengths = TestLineLengths::new();
+        let pos = Position::new(0, 0).move_up(&line_lengths, 69);
+        assert_eq!(pos, Position::new(0, 0));
+    }
+
+    #[test]
+    fn sticky_column() {
+        let mut line_lengths = TestLineLengths::new();
+        line_lengths.set(0, 10);
+        line_lengths.set(1, 0);
+        line_lengths.set(2, 20);
+        line_lengths.set(3, 6);
+
+        let pos = Position::new(3, 6).move_up(&line_lengths, 2);
+        assert_eq!(pos, Position::new_with_sticky(1, 0, 6));
+    }
+
+    #[test]
+    fn no_sticky_column_edge_case() {
+        let mut line_lengths = TestLineLengths::new();
+        line_lengths.set(0, 10);
+        line_lengths.set(1, 5);
+        line_lengths.set(2, 0);
+        line_lengths.set(3, 5);
+
+        let pos = Position::new(3, 5).move_up(&line_lengths, 2);
+        assert_eq!(pos, Position::new(1, 5));
     }
 }
