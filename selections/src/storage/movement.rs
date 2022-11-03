9d374b5d398cde2598016a500ba04c5e09acf7c8
Based 32 <based322@protonmail.com>
2022-11-04 00:19:40 +0100

    wip

--- selections/src/storage/movement.rs
+++ selections/src/storage/movement.rs
@@ -18,64 +18,61 @@ impl SelectionStorage {
             return Default::default();
         }
 
-        if let Some(idx_old) = self.find_index_by_id(id) {
-            let mut selection_new = self.selections[idx_old].move_left(&line_lengths, n, extend);
+        let Some(idx_old) = self.find_index_by_id(id) else {
+	    return Default::default();
+	};
 
-            let idx_new = self.find_overlapping_indicies_exlude(
-                &selection_new.from,
-                &selection_new.to,
-                idx_old,
-            );
+        let mut selection_new = self.selections[idx_old].move_left(&line_lengths, n, extend);
 
-            let deltas = match idx_new {
-                Err(idx) if idx == idx_old => {
-                    // Updated selection stays at the same index and no collisions to solve:
-                    let selection_old = mem::replace(&mut self.selections[idx_old], selection_new);
-                    let mut deltas = SelectionDeltas::default();
-                    deltas.push_updated(selection_old, &self.selections[idx]);
-                    deltas
+        let idx_new =
+            self.find_overlapping_indicies_exlude(&selection_new.from, &selection_new.to, idx_old);
+
+        let deltas = match idx_new {
+            Err(idx) if idx == idx_old => {
+                // Updated selection stays at the same index and no collisions to solve:
+                let selection_old = mem::replace(&mut self.selections[idx_old], selection_new);
+                let mut deltas = SelectionDeltas::default();
+                deltas.push_updated(selection_old, &self.selections[idx]);
+                deltas
+            }
+            Err(idx) => {
+                // Still no collisions, but index is changed and in case of
+                // movement to the left it will be less than before, than means
+                // a subset of all selections between old and new indexes
+                // should be moved to the right:
+                debug_assert!(idx < idx_old);
+                // Replace old selection with a new one and do rotation so new selections will
+                // be on the right of updated selection in selections storage sorted vec.
+                let selection_old = mem::replace(&mut self.selections[idx_old], selection_new);
+                self.selections[idx..=idx_old].rotate_right(1);
+                let mut deltas = SelectionDeltas::default();
+                deltas.push_updated(selection_old, &self.selections[idx]);
+                deltas
+            }
+            Ok(range) => {
+                // Collisions found, overwritten selections will be removed.
+                let start_idx = *range.start();
+                let end_idx = *range.end();
+                let mut deltas = SelectionDeltas::default();
+
+                // In case of `extend` overwritten selection must become a part of the updated
+                // selection.
+                if extend && self.selections[start_idx].from < selection_new.from {
+                    selection_new.from = self.selections[start_idx].from.clone();
                 }
-                Err(idx) => {
-                    // Still no collisions, but index is changed and in case of
-                    // movement to the left it will be less than before, than means
-                    // a subset of all selections between old and new indexes
-                    // should be moved to the right:
-                    debug_assert!(idx < idx_old);
-                    // Replace old selection with a new one and do rotation so new selections will
-                    // be on the right of updated selection in selections storage sorted vec.
-                    let selection_old = mem::replace(&mut self.selections[idx_old], selection_new);
-                    self.selections[idx..=idx_old].rotate_right(1);
-                    let mut deltas = SelectionDeltas::default();
-                    deltas.push_updated(selection_old, &self.selections[idx]);
-                    deltas
+
+                // Update a selection leaving it at the same place, as those on the left will
+                // be removed anyway.
+                let selection_old = mem::replace(&mut self.selections[idx_old], selection_new);
+                for s in self.selections.drain(start_idx..=end_idx) {
+                    deltas.push_deleted(s);
                 }
-                Ok(range) => {
-                    // Collisions found, overwritten selections will be removed.
-                    let start_idx = *range.start();
-                    let end_idx = *range.end();
-                    let mut deltas = SelectionDeltas::default();
+                deltas.push_updated(selection_old, &self.selections[start_idx]);
+                deltas
+            }
+        };
 
-                    // In case of `extend` overwritten selection must become a part of the updated
-                    // selection.
-                    if extend && self.selections[start_idx].from < selection_new.from {
-                        selection_new.from = self.selections[start_idx].from.clone();
-                    }
-
-                    // Update a selection leaving it at the same place, as those on the left will
-                    // be removed anyway.
-                    let selection_old = mem::replace(&mut self.selections[idx_old], selection_new);
-                    for s in self.selections.drain(start_idx..=end_idx) {
-                        deltas.push_deleted(s);
-                    }
-                    deltas.push_updated(selection_old, &self.selections[start_idx]);
-                    deltas
-                }
-            };
-
-            debug_assert!(self.is_state_correct());
-            deltas
-        } else {
-            return Default::default();
-        }
+        debug_assert!(self.is_state_correct());
+        deltas
     }
 }
